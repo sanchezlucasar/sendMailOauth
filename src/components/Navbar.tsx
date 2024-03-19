f16cabf96f16d145d6265419c3bb834df8c018db
sanchezlucasar <32554127+sanchezlucasar@users.noreply.github.com>
2024-03-19 11:55:30 -0300

    Se elimina el useEffect ya que se encuentra configurado el middleware de NextAuth para el manejo de  las sesiones

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,31 @@
 'use client'
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+
 
 function Navbar() {
     const { data: session } = useSession()
 
-    const router = useRouter();
-
-    useEffect(() => {
-        // Verificar si no hay sesión activa y redirigir al login
-        if (!session) {
-            router.push('/auth/login');
-        }
-    }, [session, router]);
-
     return (
-
-        <div>
-            <div className="navbar bg-slate-300 text-primary-content">
-                <div className="w-4/5 mx-auto flex items-center justify-between"> {/* Añade justify-between */}
-                    <div className="text-slate-500"> {/* Remueve flex-1 y agrega justify-start */}
-                        <a className="text-xl">SEND MAILS APP</a>
+        <>
+            <div className="navbar bg-slate-300 text-primary-content fixed w-full z-10 top-0 top-0 ">
+                <div className="w-4/5 mx-auto flex items-center justify-between">
+                    <div className="text-slate-500">
+                        <a className="text-xl">Grupo 360!</a>
                     </div>
                     <div className="flex-none mb-4">
-                        {/* Resto del contenido */}
-
                         <div className="flex-none mt-4">
                             <ul className="menu menu-horizontal text-slate-500 px-1 ">
                                 {!session?.user ? (
                                     <>
                                         <li> <Link href="/auth/login">Login</Link></li>
                                     </>
-
                                 ) : (
                                     <>
                                         <li className="">  <Link href="/dashboard">Dashboard</Link></li>
-                                        <li className="">  <Link href="/contact">Mail</Link></li>
+                                        <li className="">  <Link href="/contact">Envío de Mail</Link></li>
                                         <li className="">  <Link href="/cliente">Clientes</Link></li>
+                                        <li className="">  <Link href="/log">Registro de Mails</Link></li>
                                         <li>  <Link href="/api/auth/signout">Logout</Link></li>
                                     </>
                                 )
@@ -48,11 +35,7 @@ function Navbar() {
                     </div >
                 </div>
             </div>
-        </div >
-
-
-
-
+        </>
     )
 }
 
